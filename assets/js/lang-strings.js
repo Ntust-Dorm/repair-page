55e9247fb0ccbc46c85fc282c414f65b88cd1372
SheiUn <d215039@outlook.com>
2018-08-27 02:55:13 +0800

    update

--- assets/js/lang-strings.js
+++ assets/js/lang-strings.js
@@ -1,10 +1,14 @@
 var MLstrings = [
     {
-        English: "Repair Page for 2nd Dormitory | Taiwan Tech",
+        English: "Repair Page of 2nd Dormitory | Taiwan Tech",
         繁體中文: "第二宿舍報修頁面 | 台科"
     },
     {
-        English: "Repair Page for 2nd Dormitory",
+        English: "language:",
+        繁體中文: "語言："
+    },
+    {
+        English: "Repair Page of 2nd Dormitory",
         繁體中文: "第二宿舍報修頁面"
     },
     {
@@ -16,8 +20,8 @@ var MLstrings = [
         繁體中文: "目錄"
     },
     {
-        English: "Request Repair Service",
-        繁體中文: "報修服務"
+        English: "Request Repair",
+        繁體中文: "報修"
     },
     {
         English: "Need a repair for your room or other things?",
@@ -25,6 +29,92 @@ var MLstrings = [
     },
     {
         English: "Request repair service conveniently online.",
-        繁體中文: "需要維修你的寢室或其他東西嗎？"
+        繁體中文: "線上報修更方便。"
+    },
+    {
+        English: "Look More",
+        繁體中文: "查看更多"
+    },
+    /* Header of Table */
+    {
+        English: "Instant Preview",
+        繁體中文: "即時預覽"
+    },
+    {
+        English: "Notify the dorm center once a week.",
+        繁體中文: "每週送到宿管中心登記一次。"
+    },
+    {
+        English: "Last notifying time:",
+        繁體中文: "上次通知時間："
+    },
+    /* Table */
+    {
+        English: "Timestamp",
+        繁體中文: "時間戳記"
+    },
+    {
+        English: "Informant",
+        繁體中文: "通報人"
+    },
+    {
+        English: "Floor",
+        繁體中文: "樓層"
+    },
+    {
+        English: "Room",
+        繁體中文: "房號"
+    },
+    {
+        English: "Bed",
+        繁體中文: "床號"
+    },
+    /* Map */
+    {
+        English: "Notice",
+        繁體中文: "注意"
+    },
+    {
+        English: "If you are in hurry. Please go to dorm center directly.",
+        繁體中文: "如果你趕時間，請直接到宿管中心。"
+    },
+    {
+        English: "3rd Dormitory 1F",
+        繁體中文: "第三學生宿舍 一樓"
+    },
+    /* Dorm */
+    {
+        English: "Dormitory Cadre",
+        繁體中文: "宿舍幹部"
+    },
+    {
+        English: "If you have any question of accommodation, contact the residence advisor priority.",
+        繁體中文: "如何你有任何住宿上的問題，請優先聯絡樓長。"
+    },
+    {
+        English: "Dormitory Cadre",
+        繁體中文: "宿舍幹部"
+    },
+    /* Footer */
+    {
+        English: "If you find the bug, you can click the button to report it.",
+        繁體中文: "如果你發現錯誤，你可以點擊按鈕回報。"
+    },
+    {
+        English: "Or you want more information, just go to dorm page.",
+        繁體中文: "或者你想要看到更多的資訊，直接點擊宿舍頁面。"
+    },
+    {
+        English: "Report Bug",
+        繁體中文: "回報錯誤"
+    },
+    {
+        English: "Dorm Page",
+        繁體中文: "宿舍頁面"
+    },
+    /* Side Bar */
+    {
+        English: "Home",
+        繁體中文: "首頁"
     },
 ];
