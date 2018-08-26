1879dd378588e71cd54b02bb2b2b97dbe9bdf69d
SheiUn <d215039@outlook.com>
2018-08-26 21:07:38 +0800

    Initial commit

---
+++ assets/js/lang-codes.js
@@ -0,0 +1,14 @@
+var mlCodes = [
+    {
+        code: "zh-tw",
+        name: "繁體中文"
+    },
+    {
+        code: "zh-cn",
+        name: "简体中文"
+    },
+    {
+        code: "en",
+        name: "English"
+    },
+];
---
+++ assets/js/lang-strings.js
@@ -0,0 +1,30 @@
+var MLstrings = [
+    {
+        English: "Repair Page for 2nd Dormitory | Taiwan Tech",
+        繁體中文: "第二宿舍報修頁面 | 台科"
+    },
+    {
+        English: "Repair Page for 2nd Dormitory",
+        繁體中文: "第二宿舍報修頁面"
+    },
+    {
+        English: "Request",
+        繁體中文: "報修"
+    },
+    {
+        English: "Menu",
+        繁體中文: "目錄"
+    },
+    {
+        English: "Request Repair Service",
+        繁體中文: "報修服務"
+    },
+    {
+        English: "Need a repair for your room or other things?",
+        繁體中文: "需要維修你的寢室或其他東西嗎？"
+    },
+    {
+        English: "Request repair service conveniently online.",
+        繁體中文: "需要維修你的寢室或其他東西嗎？"
+    },
+];
