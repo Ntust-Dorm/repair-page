d0d8514225ea9ab24c33be2d09b1c0b2f77ac19a
SheiUn <me.sheiun@gmail.com>
2019-03-11 16:45:20 +0800

    chore: update cadres

--- assets/js/dorm-cadre.js
+++ assets/js/dorm-cadre.js
@@ -15,16 +15,16 @@ class Cadre {
 }
 
 var cadres = [];
-cadres.push(new Cadre("王家慶", "Dorm Manager", "409"));
-cadres.push(new Cadre("孫三合", "Dorm Vice Manager", "915"));
-cadres.push(new Cadre("李宇翔", "Resident Assistant 2F & 3F", "212"));
-cadres.push(new Cadre("黃郁峻", "Resident Assistant 4F & 5F", "415"));
-cadres.push(new Cadre("江彥成", "Resident Assistant 6F & 7F", "415"));
-cadres.push(new Cadre("劉權紘", "Resident Assistant 8F & 9F", "909"));
-cadres.push(new Cadre("徐孟辰", "Resident Assistant 10F & 11F", "415"));
-cadres.push(new Cadre("胡柏丞", "Resident Assistant 12F & 13F", "213"));
-cadres.push(new Cadre("周儀恩", "Resident Assistant 14F", "415"));
-cadres.push(new Cadre("黃偉倫", "Resident Assistant 15F", "312"));
+cadres.push(new Cadre("黃偉倫", "Dorm Manager", "312"));
+cadres.push(new Cadre("吳俊廷", "Dorm Vice Manager", "415"));
+// cadres.push(new Cadre("孫三合", "", "915"));
+// cadres.push(new Cadre("李宇翔", "Resident Assistant 2F & 3F", "212"));
+// cadres.push(new Cadre("黃郁峻", "Resident Assistant 4F & 5F", "415"));
+// cadres.push(new Cadre("江彥成", "Resident Assistant 6F & 7F", "415"));
+// cadres.push(new Cadre("劉權紘", "Resident Assistant 8F & 9F", "909"));
+// cadres.push(new Cadre("徐孟辰", "Resident Assistant 10F & 11F", "415"));
+// cadres.push(new Cadre("胡柏丞", "Resident Assistant 12F & 13F", "213"));
+// cadres.push(new Cadre("周儀恩", "Resident Assistant 14F", "415"));
 
 var cadres_strings = [];
 $.each(cadres, function (index) {
